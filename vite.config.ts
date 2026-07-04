ac47a6d93f416bf8d88892454a812e55e457d4f1
Thomas <thomasferre03@gmail.com>
2026-07-04 20:01:44 +0200

    Deploy Vite to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/mon-site/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
